509871a82d1addfea083a71f394a88b6faf3924b
unknown <theblacksword2d@gmail.com>
2024-02-01 18:23:05 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("hello world\nhello world")
+print("day 1 - string manipulation")
+print('string concatenation is done with the "+" sign.')
+print('e.g print ("hello" + "world")')
+print("New lines can be created with a backslash and n.")
+#HELLO MY MAN 😎
+print("hello " + input("what is your name my human friend ? "))
+print(len(input("what is your name ?\n   ")))
+#nice bro 😁
+a = input("a:")
+b = input("b:")
+c = a
+a = b 
+b = c
+
+print("a = " + a)
+print("b = " + b)
---
+++ project1st.py
@@ -0,0 +1,4 @@
+print("welcome to band name generator😀")
+city = input("what's name of the city you grew up in? \n")
+pit = input("what is your pet's name? \n")
+print("your band name could be " + city +' ' + pit)
